6b3d7526b5f983a6acd4c4dc83ad40c2382c761d
mrodrigues <mrodrigues.uff@gmail.com>
2015-08-27 15:52:14 -0300

    Defining default radius

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,17 +1,19 @@
 class StoresController < ApplicationController
+  DEFAULT_RADIUS = 10 # miles
+
   def index
-    @stores = Store.near(params[:address])
+    @stores = Store.near(params[:address], DEFAULT_RADIUS)
   end
 
   def search_by_city
-    @stores = Store.near(params[:city])
+    @stores = Store.near(params[:city], DEFAULT_RADIUS)
     render :index
   end
 
   def search_by_coordinates
     coordinates = [params[:latitude], params[:longitude]]
     fail "Missing coordinates" if coordinates.any?(&:blank?)
-    @stores = Store.near(coordinates)
+    @stores = Store.near(coordinates, DEFAULT_RADIUS)
     render :index
   end
 end
