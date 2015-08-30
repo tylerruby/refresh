754b39f77009cfb62d5603bbd2ba9bad587b1aa7
mrodrigues <mrodrigues.uff@gmail.com>
2015-08-30 18:19:21 -0300

    Saving user's location on session

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,18 +1,29 @@
 class StoresController < ApplicationController
   def search_by_address
     address = Geocoder.search(params[:address]).first
+    session[:coordinates] = address.coordinates
     @city = address.city
-    @stores = Store.by_city(@city).order_by_distance(address.coordinates)
+    @stores = stores
     render :index
   end
 
   def search_by_city
     @city = params[:city].titleize
-    @stores = Store.by_city(params[:city])
+    @stores = stores
     render :index
   end
 
   def show
     @store = Store.friendly.find(params[:id])
+  end
+
+  private
+
+  def stores
+    if session[:coordinates]
+      Store.by_city(@city).order_by_distance(session[:coordinates])
+    else
+      Store.by_city(params[:city])
+    end
   end
 end
