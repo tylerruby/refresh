d649c829efd5bc8d460bbb3102b02fee90916b16
mrodrigues <mrodrigues.uff@gmail.com>
2015-09-26 19:06:39 -0300

    Add logo to Chain

--- app/models/chain.rb
+++ app/models/chain.rb
@@ -1,4 +1,7 @@
 class Chain < ActiveRecord::Base
+  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" },
+    default_url: "https://placehold.it/300x300"
+  validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
   has_many :stores, dependent: :destroy
   has_many :clothes, dependent: :destroy
   validates :name, presence: true
