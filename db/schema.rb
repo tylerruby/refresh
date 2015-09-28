f500e7f524414f3dbb522a9e4cdb3391c86dc5d8
mrodrigues <mrodrigues.uff@gmail.com>
2015-09-28 01:10:06 -0300

    Authorizing user access to admin panel

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928003516) do
+ActiveRecord::Schema.define(version: 20150928035917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -165,6 +165,7 @@ ActiveRecord::Schema.define(version: 20150928003516) do
     t.string   "customer_id"
     t.string   "provider"
     t.string   "uid"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
