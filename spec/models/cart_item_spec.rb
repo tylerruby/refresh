7f36c0be903f7f5120cb89538dcd1ddc48955c6f
mrodrigues <mrodrigues.uff@gmail.com>
2015-10-03 16:01:39 -0300

    Redefining image styles for clothes and stores
    Fixed images on cart as side effect.

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "delegates attributes to item" do
+    cloth_instance = create(:cloth_instance)
+    cart_item = build(:cart_item, item: cloth_instance)
+
+    %w(name color size gender image store).each do |attribute|
+      expect(cart_item.send(attribute)).to eq cloth_instance.send(attribute)
+    end
+  end
 end
