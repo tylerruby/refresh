4b841d3582a0f6716337a9befd5c94fb04760fec
mrodrigues <mrodrigues.uff@gmail.com>
2015-11-29 23:14:12 -0200

    Creating default stores on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+atlanta = City.create!(name: 'Atlanta', state: 'GA')
+
+address = {
+  city: atlanta,
+  address: 'Georgia Tech, North Ave NW, Atlanta, GA, USA'
+}
+
+Store.create! [
+  {
+    name: 'Breakfast',
+    human_opens_at: '05:00',
+    human_closes_at: '11:00',
+    address: Address.new(address)
+  },
+  {
+    name: 'Lunch',
+    human_opens_at: '11:00',
+    human_closes_at: '18:00',
+    address: Address.new(address)
+  },
+  {
+    name: 'Dinner',
+    human_opens_at: '18:00',
+    human_closes_at: '05:00',
+    address: Address.new(address)
+  },
+  {
+    name: 'General Store',
+    address: Address.new(address)
+  }
+]
