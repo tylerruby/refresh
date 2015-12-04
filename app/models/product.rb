625f8ca8361bd422357c840b3c49689716b5f554
mrodrigues <mrodrigues.uff@gmail.com>
2015-12-04 10:31:18 -0200

    Improve the admin for Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,26 @@ class Product < ActiveRecord::Base
   validates :name, presence: true
 
   scope :available, -> { where(available: true) }
+
+  rails_admin do
+    edit do
+      field :name
+      field :description
+      field :price
+      field :store
+      field :image
+      field :available
+    end
+
+    list do
+      field :name
+      field :price do
+        pretty_value do
+          value.format
+        end
+      end
+      field :image
+      field :store
+    end
+  end
 end
