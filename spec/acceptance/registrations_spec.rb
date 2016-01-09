6adda7aa822549bc0522b9c23fcb9d45e911d4e3
mrodrigues <mrodrigues.uff@gmail.com>
2016-01-08 22:16:06 -0200

    Generating documentation for sign in/up

---
+++ spec/acceptance/registrations_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource 'Registrations' do
+  post '/users.json' do
+    parameter :email, scope: :user
+    parameter :password, scope: :user
+    let(:email) { 'some.email@example.com' }
+    let(:password) { '12345678' }
+
+    let(:token) { 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.IiI.-IZCxF2OTSg7vVCuU5MJlJ65DLGHv49LI-9YsyllVIo' }
+
+    before do
+      allow(AuthToken)
+        .to receive(:encode)
+        .with(hash_including(:user_id))
+        .and_return(token)
+    end
+
+    example "creating an user", document: :public do
+      do_request
+      expect(json).to eq(
+        "id" => User.last.id,
+        "token" => token
+      )
+    end
+
+    context "invalid password" do
+      let(:password) { '' }
+
+      example "failing to create an user", document: :public do
+        do_request
+        expect(json).to eq("password" => ["can't be blank"])
+      end
+    end
+  end
+end
