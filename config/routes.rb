6c2f5aaf82f0fb85f9e3622ff96c0784aa56b122
Tyler Ruby <tylerruby@Tylers-MacBook-Air.local>
2016-01-22 19:46:32 -0500

    Delete partner page

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,6 @@ Rails.application.routes.draw do
   get 'account' => 'pages#account'
 
   get 'help' => 'pages#help'
-  get 'partners' => 'pages#partners'
   get 'terms' => 'pages#terms'
   get 'privacy' => 'pages#privacy'
 
