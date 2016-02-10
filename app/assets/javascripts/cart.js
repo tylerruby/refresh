76b5307d4e6050061b2532de627b50d52d37e713
mrodrigues <mrodrigues.uff@gmail.com>
2016-02-10 18:14:29 -0200

    Close cart if empty

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -5,9 +5,13 @@ $(function setupCart() {
   var drawer = $('.drawer');
   setupBindings(cart);
 
+  function close() {
+    drawer.drawer('close');
+  }
+
   function setupBindings(cart) {
     cart.find('.close').click(function closeCart() {
-      drawer.drawer('close');
+      close();
     });
     cart.find('form:has(.remove-item-button)').submit(removeAsync);
     cart.find('form:has(.update-quantity)').submit(updateQuantityAsync);
@@ -16,6 +20,7 @@ $(function setupCart() {
     $('.cart-icon .badge, .cart-items-quantity').text(cartItemsCount);
     if (cartItemsCount == 0) {
       cartIconMenu.addClass("hide");
+      close();
     } else {
       cartIconMenu.removeClass("hide");
     }
