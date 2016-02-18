02e0ed3db560007c342412e9c40eec3fd58d92a0
mrodrigues <mrodrigues.uff@gmail.com>
2016-02-18 19:03:04 -0200

    Redirect and alert user if he doesn't have a current address

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,5 +1,20 @@
 class MenusController < ApplicationController
   def show
+    unless current_address.present?
+      return respond_to do |format|
+        format.json do
+          render json: {
+            message: "You should choose a current address first."
+          }, status: :unprocessable_entity
+        end
+
+        format.html do
+          flash[:alert] = "You should choose a current address first."
+          redirect_to root_path
+        end
+      end
+    end
+
     @selected_date = (params[:date] || Date.current).to_date
     @menu = Menu.find_by(
       date: @selected_date,
