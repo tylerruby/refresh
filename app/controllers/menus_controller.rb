34dd37c7d7dadb68e038339aea60f249813050b9
mrodrigues <mrodrigues.uff@gmail.com>
2016-02-18 18:46:18 -0200

    Show message when there's no menu for that date and region

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,9 +1,17 @@
 class MenusController < ApplicationController
   def show
-    @menu = Menu.find_by!(
-      date: params[:date] || Date.current,
-      region: SelectRegion.new(current_address).region
+    @selected_date = (params[:date] || Date.current).to_date
+    @menu = Menu.find_by(
+      date: @selected_date,
+      region: user_region
     )
     @menu_dates = SelectMenuDates.new(Date.current).dates
+    return render(:not_found, status: :unprocessable_entity) unless @menu.present?
   end
+
+  private
+
+    def user_region
+      SelectRegion.new(current_address).region
+    end
 end
