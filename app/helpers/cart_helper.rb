2c3c937c93520493ffb8d7220081333064697d6d
mrodrigues <mrodrigues.uff@gmail.com>
2016-02-23 20:01:00 -0300

    Fixing checkout as a guest

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -1,12 +1,10 @@
 module CartHelper
   def update_quantity_button(cart_item, increment)
     button_to(
-      {
-        controller: :cart,
-        action: :update,
+      cart_update_path(
         quantity: cart_item.quantity + increment,
         menu_product_id: cart_item.item_id
-      },
+      ),
       method: :patch,
       class: 'update-quantity'
     ) do
